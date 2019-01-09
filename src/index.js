f5dfe6b4735c9e2caafae0f4f0ce29ef983e55a6
Brandon lent <blent1050@yahoo.com>
2019-01-09 12:23:52 -0500

    Creating react components with map

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './styles.css';
+import "./styles.css";
 
 const disneyData = [
-  { characterName: 'Mickey Mouse' },
-  { characterName: 'Minnie Mouse' },
-  { characterName: 'Donald Duck' },
-  { characterName: 'Goofy' },
-  { characterName: 'Pluto' }
+  { characterName: "Mickey Mouse" },
+  { characterName: "Minnie Mouse" },
+  { characterName: "Donald Duck" },
+  { characterName: "Goofy" },
+  { characterName: "Pluto" }
 ];
 
 function App() {
-  return <div className="App" />;
+  const mappedData = disneyData.map(character => {
+    return <div>{character.characterName}</div>;
+  });
+  return <div className="App">{mappedData}</div>;
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
